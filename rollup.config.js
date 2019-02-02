b131bb8a909117e051cfd6d2cfd4a18053d5f2f2
Mohammad Aziz <mdaziz067@gmail.com>
2019-02-02 23:32:58 +0530

    initial rollup config

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const babel = require('rollup-plugin-babel');
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx', '.ejs'];
+
+module.exports = {
+  input: path.join(__dirname, 'src', 'index.tsx'),
+  output: {
+    dir: 'dist',
+    exports: 'named',
+    format: 'umd',
+    globals: { react: 'React', 'styled-components': 'styled' },
+    name: 'HoverCard'
+  },
+  external: ['react', 'styled-components'],
+  plugins: [
+    resolve({
+      extensions
+    }),
+    babel({
+      extensions
+    }),
+    commonjs()
+  ]
+};
