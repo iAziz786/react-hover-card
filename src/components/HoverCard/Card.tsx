ec3122a2f715e0351f03a8f3ed3ea0390aad7341
Mohammad Aziz <mdaziz067@gmail.com>
2019-02-04 21:17:18 +0530

    Update styling of Card component

--- src/components/HoverCard/Card.tsx
+++ src/components/HoverCard/Card.tsx
@@ -2,6 +2,9 @@ import * as React from "react"
 import styled from "styled-components"
 
 const Card = styled.div`
+  background-color: white;
+  color: #000;
+  border-radius: 5px;
   padding: 10px;
   position: absolute;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
